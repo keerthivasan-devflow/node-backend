6e1a26ae7efe49fabee137d3cbaa75118de4a36a
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-01-06 15:31:51 +0530

    Created a app.js file to run javascript code and learned about 'global' keyword

---
+++ app.js
@@ -0,0 +1,8 @@
+var username = "Keerthivasan";
+let number = 174640;
+console.log(username);
+console.log(number);
+
+// You can get global object via 'global' keyword
+console.log(global);
+console.log(this);
