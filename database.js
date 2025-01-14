2abba1206f3c1e2fcb09c0b2a8fe110460fb4143
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-01-15 01:29:03 +0530

    Updated database.js and study notes after hiding the credentials of mongodb.

---
+++ database.js
@@ -0,0 +1,55 @@
+const { MongoClient } = require("mongodb");
+
+const CONNECTION_STRING_URI =
+  "mongodb+srv://keerthivasan:********@nodestore.hjjeh.mongodb.net/";
+
+const client = new MongoClient(CONNECTION_STRING_URI);
+
+const dbname = "college";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbname);
+  const collection = db.collection("students");
+
+  // the following code examples can be pasted here...
+  const singleRecordToInsert = {
+    studentid: "S174642",
+    firstname: "Gurram",
+    lastname: "Harika",
+    fullname: "Gurram Harika",
+    dateofbirth: "1997-08-12",
+    gender: "female",
+    email: "gurram.harika@gmail.com",
+    phonenumber: "+91-9093877123",
+    address: {
+      street: "Lakshmi Nagar",
+      city: "Brookfield",
+      state: "KN",
+      zipcode: "560036",
+    },
+    status: "active",
+    graduationyear: 2018,
+  };
+
+  // Commented the insert document code otherwise everytime when you run your code, it will insert the document
+  // const InsertResult = await collection.insertOne(singleRecordToInsert);
+  // console.log("Record inserted successfully", InsertResult);
+
+  const student = await collection
+    .find({ graduationyear: 2018 })
+    .toArray();
+  console.log(student);
+
+  const countDocuments = await collection.countDocuments();
+  console.log("Count of Documents: ", countDocuments);
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
---
+++ study-notes/12-Creating-Database.md
@@ -0,0 +1,16 @@
+## MongoDB Enterprise Edition Free Setup / Installation of MongoDB Compass / Core Concepts Of `mongodb` Library
+- mongodb official: https://cloud.mongodb.com/
+- npm mongodb: https://www.npmjs.com/package/mongodb
+- mongodb driver: https://mongodb.github.io/node-mongodb-native/
+- mongodb driver API: https://mongodb.github.io/node-mongodb-native/6.12/
+
+**Note:** mongodb package will be used only for learning purpose only. Not recommended to use it in production build
+Rather it's always better to go with `mongoose library`
+
+1. Go to official mongodb site
+2. Create an account
+3. Create a cluster
+4. Once cluster created, get the connection string
+    - **connection string:** mongodb+srv://keerthivasan:********@nodestore.hjjeh.mongodb.net/
+
+5. Install Mongo DB Compass
